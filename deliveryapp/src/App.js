8576be3a37df81b19587f83c79d72497b655b263
Midecodes <faginorish@gmail.com>
2020-07-19 17:57:17 +0100

    Consume API for logistics

---
+++ deliveryapp/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Orders from './Components/Orders';
+
+function App() {
+  return (
+    <div className="App">
+     <Orders/>
+    </div>
+  );
+}
+
+export default App;
